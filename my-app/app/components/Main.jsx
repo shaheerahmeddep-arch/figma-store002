911f9c50b42765caf0f5008661272409e40c22f1
shaheerahmeddep-arch <shaheerahmed.dep@gmail.com>
2026-02-12 19:03:37 +0500

    Reapply "Initial commit"
    This reverts commit b8ae21946d9d7e4ba6c516ecfd7dfc959ad049e1.

---
+++ my-app/app/components/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+    <div className='flex'>
+        <div className='flex'>
+            <ul className='pt-10 ml-33'>
+                <li className='flex items-center gap-12'>Woman’s Fashion <img className='' src="Arrow.png" alt="" /></li>
+                <li className='flex items-center gap-18 mt-4'>Men’s Fashion <img className='' src="Arrow.png" alt="" /></li>
+                <li className='mt-4'>Electronics</li>
+                <li className='mt-4'>Home & Lifestyle</li>
+                <li className='mt-4'>Medicine</li>
+                <li className='mt-4'>Sports & Outdoor</li>
+                <li className='mt-4'>Baby’s & Toys</li>
+                <li className='mt-4'>Groceries & Pets</li>
+                <li className='mt-4'>Health & Beauty</li>
+            </ul>
+<div class="w-px bg-[#969696] h-96 ml-6"></div>
+
+        </div>
+        <div>
+            <img className='h-90 pt-10 ml-12' src="Frame.png" alt="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
---
+++ my-app/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Main1 from "./components/Main1";
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Main/>
+    <Main1/>
+    </>
+  );
+}
